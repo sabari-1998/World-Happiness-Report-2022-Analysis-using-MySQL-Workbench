876745c2e67ed73051431724d8dcc372fd9e0a80
sabari-1998 <35098280+sabari-1998@users.noreply.github.com>
2023-07-10 22:16:24 +0530

    Add files via upload

---
+++ world_happiness.sql
@@ -0,0 +1,104 @@
+-- To load the databse
+USE world_happiness; 
+SELECT * 
+FROM data2022;
+
+-- Count the number of columns for better understanding
+SELECT count(*) AS NumberOfColumns
+FROM information_schema.columns
+WHERE table_name = 'data2022';
+
+-- Now let's find out the number of rows.
+SELECT COUNT(*) AS NumberOfRows
+FROM data2022;
+
+-- To get more data information
+SHOW columns FROM `data2022`;
+
+-- Checking null values in happiness_score column
+SELECT *
+FROM data2022
+WHERE Happiness_score IS NULL OR Happiness_score = ''; 
+
+-- Deleting the last record with null value for all features
+-- Disabling the safe_update mode first 
+SET SQL_SAFE_UPDATES=0;
+-- execute delete statement
+DELETE FROM data2022
+WHERE Country = 'XX';
+-- enable safe update mode
+SET SQL_SAFE_UPDATES=1;
+
+-- Analyzing the data
+-- Top 5 countries with highest score
+SELECT ID_RANK, Country, Happiness_score
+From data2022
+order by Happiness_score desc
+limit 5;
+
+-- Top 5 countries with least score
+SELECT ID_RANK, Country, Happiness_score
+From data2022
+order by Happiness_score asc
+limit 5;
+
+-- SCORE and RANK of INDIA
+SELECT ID_RANK, Country, Happiness_score
+FROM data2022
+WHERE Country = 'italy';
+
+-- Top 5 countries in GDP_pc
+SELECT Country, GDP_pc
+FROM data2022
+order by GDP_pc desc
+LIMIT 5;
+
+-- Top 5 countries in Social_support
+SELECT Country, Social_support
+FROM data2022
+order by Social_support desc
+LIMIT 5;
+
+-- Exploring the min, max and avg Health_life_expectancy score
+SELECT ID_RANK, Country, Healthy_life_expectancy as min_Health_Life_Exp
+FROM data2022
+group by Country,ID_RANK,Healthy_life_expectancy
+Having min(Healthy_life_expectancy)
+order by min(Healthy_life_expectancy) asc
+Limit 5;
+
+SELECT Country, Healthy_life_expectancy
+FROM data2022
+group by Country,Healthy_life_expectancy
+Having max(Healthy_life_expectancy)
+order by max(Healthy_life_expectancy) desc
+Limit 5;
+
+SELECT round(avg(Healthy_life_expectancy), 2)
+FROM data2022;
+
+-- Top 5 countries in freedom_to_choose
+SELECT ID_RANK, Country, freedom_to_choose
+FROM data2022
+Group by ID_RANK,Country,freedom_to_choose
+Order by freedom_to_choose desc
+Limit 5;
+
+-- Top 5 countries in Generosity
+SELECT ID_RANK, Country, Generosity
+FROM data2022
+order by Generosity desc
+LIMIT 5;
+
+-- Top 5 countries with highest perception_of_corruption
+SELECT Country, Perceptions_of_corruption
+FROM data2022
+order by Perceptions_of_corruption desc
+LIMIT 5;
+
+-- Top 5 countries with lowest perception_of_corruption
+SELECT Country, Perceptions_of_corruption
+FROM data2022
+order by Perceptions_of_corruption asc
+LIMIT 5;
+
